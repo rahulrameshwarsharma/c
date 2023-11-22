56f56e6a23c10b095efdfa7c566c5f1d4a1592af
rahulrameshwarsharma <rahul.rameshwar.sharma@gmail.com>
2023-11-22 19:43:27 +0530

    first commit

---
+++ hello.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{   
+    int x;
+    int y;
+    printf("Value of X:");
+    scanf ("value of x\n: %i", x);
+    printf("Value of Y:");
+    scanf ("value of y: %i", y);
+    printf ("sum %i%i", x + y);
+    printf ("\n");
+    printf("hello, world, this is me/n");
+}
